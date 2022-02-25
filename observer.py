4429d2fbd640e53d0d4437a9ed619540bfdfdead
Daniel Chew <cctdaniel@outlook.com>
2022-02-25 11:42:05 +0800

    skip alerts for ignored symbols (#22)
    * skip alerts for ignored symbols
    
    * use command line args instead and fix logging issues with StoppedPublishing event

--- observer.py
+++ observer.py
@@ -34,6 +34,7 @@ def get_publishers(network):
 
 
 async def main(args):
+    symbols_to_ignore = set(args.ignore) if args.ignore else set()
     program_key = get_key(network=args.network, type="program", version="v2")
     mapping_key = get_key(network=args.network, type="mapping", version="v2")
     http_url, ws_url = get_solana_urls(network=args.network)
@@ -74,61 +75,64 @@ async def main(args):
                 for product in products:
                     errors = []
                     symbol = product.symbol
-                    coingecko_price = coingecko_prices.get(product.attrs['base'])
 
-                    if symbol not in validators:
-                        # TODO: If publisher_key is not None, then only do validation for that publisher
-                        validators[symbol] = PriceValidator(
-                            key=args.publisher_key,
-                            network=args.network,
-                            symbol=symbol,
-                            coingecko_price=coingecko_price
+                    # only perform checks for symbols that are not ignored
+                    if symbol not in symbols_to_ignore:
+                        coingecko_price = coingecko_prices.get(product.attrs['base'])
+                        # prevent adding duplicate symbols
+                        if symbol not in validators:
+                            # TODO: If publisher_key is not None, then only do validation for that publisher
+                            validators[symbol] = PriceValidator(
+                                key=args.publisher_key,
+                                network=args.network,
+                                symbol=symbol,
+                                coingecko_price=coingecko_price
+                            )
+
+                        prices = await product.get_prices()
+
+                        for _, price_account in prices.items():
+                            price = Price(
+                                slot=price_account.slot,
+                                aggregate=price_account.aggregate_price_info,
+                                product_attrs=product.attrs,
+                                publishers=publishers,
+                            )
+                            price_account_errors = validators[symbol].verify_price_account(
+                                price_account=price_account,
+                            )
+                            if price_account_errors:
+                                errors.extend(price_account_errors)
+
+                            for price_comp in price_account.price_components:
+                                # The PythPublisherKey
+                                publisher = price_comp.publisher_key.key
+
+                                price.quoters[publisher] = price_comp.latest_price_info
+                                price.quoter_aggregates[
+                                    publisher
+                                ] = price_comp.last_aggregate_price_info
+
+                                if args.enable_prometheus:
+                                    gprice.labels(
+                                        symbol=symbol,
+                                        publisher=publisher,
+                                        status=price.quoters[publisher].price_status.name,
+                                    ).set(price.quoters[publisher].price)
+
+                            # Where the magic happens!
+                            price_errors = validators[symbol].verify_price(
+                                price=price, include_noisy=args.include_noisy_alerts
+                            )
+                            if price_errors:
+                                errors.extend(price_errors)
+
+                        # Send all notifications for a given symbol pair
+                        await validators[symbol].notify(
+                            errors,
+                            slack_webhook_url=args.slack_webhook_url,
+                            notification_mins=args.notification_snooze_mins,
                         )
-
-                    prices = await product.get_prices()
-
-                    for _, price_account in prices.items():
-                        price = Price(
-                            slot=price_account.slot,
-                            aggregate=price_account.aggregate_price_info,
-                            product_attrs=product.attrs,
-                            publishers=publishers,
-                        )
-                        price_account_errors = validators[symbol].verify_price_account(
-                            price_account=price_account,
-                        )
-                        if price_account_errors:
-                            errors.extend(price_account_errors)
-
-                        for price_comp in price_account.price_components:
-                            # The PythPublisherKey
-                            publisher = price_comp.publisher_key.key
-
-                            price.quoters[publisher] = price_comp.latest_price_info
-                            price.quoter_aggregates[
-                                publisher
-                            ] = price_comp.last_aggregate_price_info
-
-                            if args.enable_prometheus:
-                                gprice.labels(
-                                    symbol=symbol,
-                                    publisher=publisher,
-                                    status=price.quoters[publisher].price_status.name,
-                                ).set(price.quoters[publisher].price)
-
-                        # Where the magic happens!
-                        price_errors = validators[symbol].verify_price(
-                            price=price, include_noisy=args.include_noisy_alerts
-                        )
-                        if price_errors:
-                            errors.extend(price_errors)
-
-                    # Send all notifications for a given symbol pair
-                    await validators[symbol].notify(
-                        errors,
-                        slack_webhook_url=args.slack_webhook_url,
-                        notification_mins=args.notification_snooze_mins,
-                    )
                 await asyncio.sleep(0.4)
 
     async def run_coingecko_get_price():
@@ -201,6 +205,12 @@ if __name__ == "__main__":
         type=int,
         default=9001,
         help="Prometheus Exporter port",
+    )
+    parser.add_argument(
+        "-i",
+        "--ignore",
+        nargs="+",
+        help="List of symbols to ignore",
     )
     args = parser.parse_args()
 
