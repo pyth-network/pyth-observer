8ae7075452b56859f4584fae70de9cfba9394aff
Jayant Krishnamurthy <jayantkrishnamurthy@gmail.com>
2022-09-26 06:51:41 -0700

    Send notifications as Datadog Events (#41)
    * datadog events
    
    * stuff
    
    * datadog
    
    * fix
    
    * aggregation key
    
    * cleanup
    
    * grrr
    
    Co-authored-by: Jayant Krishnamurthy <jkrishnamurthy@jumptrading.com>
    Co-authored-by: Jayant Krishnamurthy <jayant@jumpcrypto.com>

---
+++ pyth_observer/notifiers/datadog.py
@@ -0,0 +1,34 @@
+from .notification import NotificationBase
+
+from datadog_api_client import ApiClient, Configuration
+from datadog_api_client.v1.api.events_api import EventsApi
+from datadog_api_client.v1.model.event_create_request import EventCreateRequest
+
+
+class Notifier(NotificationBase):
+    """
+    Notify datadog via its events API
+    """
+
+    def __init__(self, _unused):
+        self.configuration = Configuration()
+
+    async def notify(self, error):
+        title, details = error.get_event_details()
+
+        error_id = error.unique_id
+
+        tags = [f"symbol:{error.symbol}", f"error_code:{error.error_code}", f"network:{error.network}"]
+        if error.publisher_key is not None:
+            tags.append(f"publisher:{error.publisher_name}")
+
+        body = EventCreateRequest(
+            title=title,
+            text="\n".join(details),
+            tags=tags,
+            aggregation_key=error_id,
+        )
+
+        with ApiClient(self.configuration) as api_client:
+            api_instance = EventsApi(api_client)
+            api_instance.create_event(body=body)
